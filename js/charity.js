3618fbee8e810652b464f188632422e1f35d50e5
Nguyen Dang Cuong <you@example.com>
2022-08-09 09:36:51 +0700

    up

---
+++ js/charity.js
@@ -0,0 +1,19 @@
+function who() {
+    return window.location.href = "charity.html";
+ }
+ function cross() {
+    window.location.href = "charity.html";
+ }
+ function children() {
+    window.location.href = "charity.html";
+ }
+ function plan() {
+    window.location.href = "charity.html";
+ }
+ function unicef() {
+    window.location.href = "charity.html";
+ }
+ function maiam() {
+    window.location.href = "charity.html";
+ }
+
