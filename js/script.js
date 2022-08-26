b61eb2c7c6577a1482c4e2cd529f42e3328c43ac
in4koi <cuong.nguyendang801@gmail.com>
2022-08-26 15:56:26 +0700

    fuck

--- js/script.js
+++ js/script.js
@@ -187,9 +187,9 @@ function startTimer(time){
 function startTimerLine(time){
     counterLine = setInterval(timer, 45);
     function timer(){
-        time += 0.7; //upgrading time value with 1
+        time += 1; //upgrading time value with 1
         time_line.style.width = time + "px"; //increasing width of time_line with px by time value
-        if(time > 549){ //if time value is greater than 549
+        if(time > 1000){ //if time value is greater than 549
             clearInterval(counterLine); //clear counterLine
         }
     }
